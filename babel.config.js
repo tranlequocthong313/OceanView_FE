13231f9be21969fb2f85752a9ba7c9eb254005c4
tranlequocthong313 <tranlequocthong313@example.com>
2024-04-03 22:16:12 +0700

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = (api) => {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                require.resolve('babel-plugin-module-resolver'),
+                {
+                    root: ['./src/'],
+                    alias: {
+                        '~': './src',
+                    },
+                },
+            ],
+        ],
+    };
 };
