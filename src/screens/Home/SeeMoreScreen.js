a059451ce84f387681112a507107eeffef2d4986
minhha2k3 <hadep7a@gmail.com>
2024-05-17 01:07:40 +0700

    style: css locker, lockerdetail, ..

--- src/screens/Home/SeeMoreScreen.js
+++ src/screens/Home/SeeMoreScreen.js
@@ -1,13 +1,16 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import UtilityScreen from './UtilityScreen';
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        flex: 1,
+    },
 });
 
-export default function SeeMoreScreen({  }) {
+export default function SeeMoreScreen() {
     return (
         <View style={styles.container}>
+            <Text>SeeMore Screen</Text>
             <UtilityScreen />
         </View>
     );
